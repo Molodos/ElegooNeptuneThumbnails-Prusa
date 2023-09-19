73ecbfffcaec4c24f70c33b7a1a687aa53303d9e
Molodos <michael-weichenrieder@t-online.de>
2023-09-19 17:36:49 +0200

    Fixed the last bug, that caused thumbnails to be displayed wrongly on Neptune 2, Neptune 2S, Neptune 2D and Neptune X printers

--- elegoo_neptune_thumbnails.py
+++ elegoo_neptune_thumbnails.py
@@ -111,7 +111,7 @@ class ElegooNeptuneThumbnails:
         # Parse to g-code prefix
         gcode_prefix: str = ""
         if self._is_old_thumbnail():
-            gcode_prefix += self._parse_thumbnail_old(self._thumbnail, 160, 160, "simage")
+            gcode_prefix += self._parse_thumbnail_old(self._thumbnail, 100, 100, "simage")
             gcode_prefix += self._parse_thumbnail_old(self._thumbnail, 200, 200, ";gimage")
         elif self._is_new_thumbnail():
             gcode_prefix += self._parse_thumbnail_new(self._thumbnail, 200, 200, "gimage")
